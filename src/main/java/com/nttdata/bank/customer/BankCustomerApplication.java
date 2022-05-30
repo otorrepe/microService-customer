5cc2f3c7564382c616fd7ee75fe29a5a02bab5e4
Gian Castro Morales <jesusgianmarco.castromorales@everis.nttdata.com>
2022-05-30 10:23:21 -0500

    commit

--- src/main/java/com/nttdata/bank/customer/BankCustomerApplication.java
+++ src/main/java/com/nttdata/bank/customer/BankCustomerApplication.java
@@ -2,10 +2,12 @@ package com.nttdata.bank.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableReactiveFeignClients
 public class BankCustomerApplication {
 
